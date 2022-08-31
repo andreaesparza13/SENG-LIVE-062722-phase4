1120111b260252cd387abec2c19910a92f50fc3c
Andrea Esparza <andreaesparza13@users.noreply.github.com>
2022-08-31 14:34:52 -0500

    lecture 3

--- 03_client_server_communication/flatiron_theater_company/app/controllers/productions_controller.rb
+++ 03_client_server_communication/flatiron_theater_company/app/controllers/productions_controller.rb
@@ -7,35 +7,29 @@ class ProductionsController < ApplicationController
     def show
         production = Production.find(params[:id])
         render json: production, status: :ok
-        rescue ActiveRecord::RecordNotFound => error
-            render json: {message: error.message}
     end
 
     def create
-        production = Production.create(production_params)
-        render json: production, status: :created
+        render json: Production.create!(production_params), status: :created
     end
 
     def update
-        production = Production.find(params[:id])
-        production.update(production_params)
-        render json: production, status: :accepted
-        rescue ActiveRecord::RecordNotFound => error
-            render json: {message: error.message}
+        render json: find_prod.update!(production_params), status: :accepted
     end
 
     def destroy
-        production = Production.find(params[:id])
-        production.destroy
+        find_prod.destroy
         head :no_content
-        rescue ActiveRecord::RecordNotFound => error
-            render json: {message: error.message}
     end
 
     private
 
     def production_params
         params.permit(:title, :genre, :description, :budget, :image, :director, :ongoing)
+    end
+
+    def find_prod
+        @prod = Production.find(params[:id])
     end
 
 end
--- 03_client_server_communication/flatiron_theater_company/app/models/production.rb
+++ 03_client_server_communication/flatiron_theater_company/app/models/production.rb
@@ -1,3 +1,4 @@
 class Production < ApplicationRecord
-
+   validates :title, :director, presence: true
+   validates :budget, numericality: { greater_than: 0 }
 end
