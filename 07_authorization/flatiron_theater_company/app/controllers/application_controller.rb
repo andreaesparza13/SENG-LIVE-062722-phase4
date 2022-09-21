135a6e65edca9b3428784bac4eb14cc0b969dd10
Andrea Esparza <andreaesparza13@users.noreply.github.com>
2022-09-21 14:53:25 -0500

    lecture 6 and 7

--- 07_authorization/flatiron_theater_company/app/controllers/application_controller.rb
+++ 07_authorization/flatiron_theater_company/app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    before_action :authenticate_user
 
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
     private
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
 
     def render_unprocessable_entity(invalid)
         render json: {errors: invalid.record.errors}, status: :unprocessable_entity
@@ -13,4 +18,13 @@ class ApplicationController < ActionController::API
      def render_not_found(error)
         render json: {errors: {error.model => "Not Found"}}, status: :not_found
     end 
+
+    def authenticate_user
+        render json: { errors: "Not Authorized" }, status: :unauthorized unless current_user
+    end
+
+    def is_authorized? 
+        permitted = current_user.admin?
+        render json: { errors: "User does not have admin permission" }, status: :forbidden unless permitted
+    end
 end
--- 07_authorization/flatiron_theater_company/app/controllers/users_controller.rb
+++ 07_authorization/flatiron_theater_company/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    skip_before_action :authenticate_user, except: :show
     def show 
         user = User.find(params[:id])
         render json: user, status: :ok
